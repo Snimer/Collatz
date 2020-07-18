4f789383c34bc05c8b25004652363121d953026a
Snimerjot Singh <63850556+Snimer@users.noreply.github.com>
2020-07-18 13:13:39 +0530

    Add files via upload

---
+++ collatz.py
@@ -0,0 +1,18 @@
+from time import sleep
+def collatz(num):
+    while num > 1:
+        print(num)
+        if num % 2 == 0:
+            num = num // 2
+            sleep(0.5)
+        elif num % 2 == 1:
+            num = 3 * num + 1
+            sleep(0.25)
+    print(1)
+
+def main():
+    number = int(input('Enter a Number: '))
+    print('Collatz Sequence: ')
+    collatz(number)
+
+main()
